5d8c78d7a6397144ce6528cf83aeff17d8e4d427
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-02 16:35:14 +0200

    Attempt a boolean

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import redis from 'redis';
 import { promisify } from 'util';
 
@@ -12,34 +13,22 @@ class RedisClient {
         this.client.on('error', (error) => {
             console.log(`Redis client not connected to the server: ${error.message}`);
         });
-
-        // Ensure the client is connected before any operation
-        this.connectionPromise = new Promise((resolve) => {
-            this.client.on('connect', () => {
-                console.log('Redis client connected to the server');
-                resolve();
-            });
-        });
     }
 
-    async isAlive() {
-        await this.connectionPromise;
+    isAlive() {
         return this.client.connected;
     }
 
     async get(key) {
-        await this.connectionPromise;
         return this.getAsync(key);
     }
 
     async set(key, value, duration) {
-        await this.connectionPromise;
         await this.setAsync(key, value);
         return this.expireAsync(key, duration);
     }
 
     async del(key) {
-        await this.connectionPromise;
         return this.delAsync(key);
     }
 }
