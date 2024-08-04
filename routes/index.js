d7daa0561f9488bd0a5d7f2a58c198b882e8f306
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-04 16:35:00 +0200

    update route and create userscontroller

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 /* eslint-disable */
 import express from 'express';
 import AppController from '../controllers/AppController.js';
+import UserController from '../controller'
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('users',UsersController.postNew);
+router.post('/users', UsersController.postNew);
 
 export default router;
