528535fad6862bcb746421251d6d567473051e01
HolbyKate <cathy.augustin@holbertonstudents.com>
2024-08-04 21:48:19 +0200

    Update Usercontroller with GET /users/me

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -35,6 +35,29 @@ class UsersController {
             email,
         });
     }
+
+    static async getMe(req, res) {
+        const token = req.headers['x-token'];
+        if (!token) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const key = `auth_${token}`;
+        const userId = await redisClient.get(key);
+
+        if (!userId) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const usersCollection = dbClient.db.collection('users');
+        const user = await usersCollection.findOne({ _id: new ObjectId(userId) });
+
+        if (!user) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        return res.status(200).json({ id: user._id.toString(), email: user.email });
+    }
 }
 
 export default UsersController;
